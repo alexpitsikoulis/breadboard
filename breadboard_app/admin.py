8152bc94c0b32a07da3def3725704a7050766a4a
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-08-12 13:50:20 -0400

    registered models in admin.py

--- breadboard_app/admin.py
+++ breadboard_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Project, Component, Comment
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Component)
+admin.site.register(Comment)
